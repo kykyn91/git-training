baa4af91dc5a403a8c032b3d2f952ea9c8d0bae6
Nur Sahira <nursahiraizam@gmail.com>
2024-02-07 15:30:26 +0800

    ll

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <?php
 
-<<<<<<< HEAD
+
 echo "Aiman Handsome";
-<<<<<<< HEAD
+
 echo "Eira";
-=======
+echo "Piye CHATGPT";
+
 
 echo "Jimi Neutron";
-=====
+
 echo "Piye ensem";
->>>>>>> 097a85d7639fac1758899cd3fd107f8ce058fe9a
+
 echo "Rashdan Power";
 >>>>>>> c58bfc7f4cac986d91f11a939b16875c797d5269
 echo "Eira Syomel";
